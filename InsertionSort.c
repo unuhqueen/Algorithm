569f7917419a0ab1579d97b8a78491f14c775e65
최승원 <csw98213@gmail.com>
2019-12-26 01:21:43 +0900

    insertionSort
    Study about insertion sort

---
+++ InsertionSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int i, j, temp;
+	int arr[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	
+	for(i = 0; i < 9; i++){
+		j = i;
+		while(arr[j] > arr[j + 1]){
+			temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+			j--;
+		}
+	}
+
+	
+	for(i = 0; i < 10; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
+}
