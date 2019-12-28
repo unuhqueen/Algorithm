4e5d007019c6bf4741a04354d7f8507b226cc0a3
최승원 <csw213@naver.com>
2019-12-29 02:02:54 +0900

    quickSort
    study about quick sort

---
+++ quickSort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int number = 10;
+int data[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+
+void quickSort(int *data, int start, int end){
+	if(start >= end){ // 첫번째 원소 순서가 마지막 원소 순서보다 크거나 같으므로 원소가 1개인 경우 
+		return;
+	}
+	
+	int key = start; // 키는 첫번째 원소
+	int i = start + 1;
+	int j = end;
+	int temp;
+	
+	while(i <= j){ // 엇갈릴 때까지 반복 
+		while(data[i] <= data[key]){ //키 값보다 큰 값을 만날때까지 반복 
+			i++;
+		}
+		while(data[j] >= data[key] && j > start){ //키 값보다 작은 값을 만날때까지 반복 
+		
+		}
+		if(i > j){ //현재 엇갈린 상태면 키값과 교체 
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		} else {
+			temp = data[j];
+			data[i] = data[j];
+			data[j] = temp;
+		}
+		
+		quickSort(data, start, j - 1);
+		quickSort(data, j + 1, end);		
+	}
+}
+
+int main(){
+ 	quickSort(data, 0, number - 1);
+ 	for(int i = 0; i < number; i++){
+		printf("%d", data[i]);
+	}
+	 
+}
