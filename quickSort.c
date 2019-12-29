ba9188bfacb6e84df1bedb7dae7ef73edcd39cec
최승원 <csw213@naver.com>
2019-12-30 03:34:53 +0900

    quick sort(내림차순)
    quick sort 코드를 수정하여 내림차순 코드로 변경

--- quickSort.c
+++ quickSort.c
@@ -13,18 +13,18 @@ void quickSort(int *data, int start, int end){
 	int temp;
 	
 	while(i <= j){ // 엇갈릴 때까지 반복 
-		while(data[i] <= data[key]){ //키 값보다 큰 값을 만날때까지 반복 
+		while(data[i] >= data[key] && i <= end){ //키 값보다 큰 값을 만날때까지 반복 
 			i++;
 		}
-		while(data[j] >= data[key] && j > start){ //키 값보다 작은 값을 만날때까지 반복 
-		
+		while(data[j] <= data[key] && j > start){ //키 값보다 작은 값을 만날때까지 반복 
+			j--;
 		}
 		if(i > j){ //현재 엇갈린 상태면 키값과 교체 
 			temp = data[j];
 			data[j] = data[key];
 			data[key] = temp;
 		} else {
-			temp = data[j];
+			temp = data[i];
 			data[i] = data[j];
 			data[j] = temp;
 		}
@@ -36,7 +36,8 @@ void quickSort(int *data, int start, int end){
 
 int main(){
  	quickSort(data, 0, number - 1);
- 	for(int i = 0; i < number; i++){
+ 	int i;
+	for(i = 0; i < number; i++){
 		printf("%d", data[i]);
 	}
 	 
