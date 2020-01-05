92cbf9da93b3f42fe3ed881485e4992e44f7497d
최승원 <csw213@naver.com>
2020-01-06 03:24:40 +0900

    code successsss
    complete

--- findMax.c
+++ findMax.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-int findMax(int arr[], int n, int max){
-	if(n == 0) return;
-	else{
-		if()
-	}
-	findMax(arr, n - 1);
+int findMax(int arr[], int n){
+	int max;
+	if(n == 0) return arr[0];
+	if(arr[n - 1] > findMax(arr, n - 1)) return arr[n-1];
+	
 }
 
 int main(){
-	int i, n, arr[20], max = 0;
+	int i, n, arr[20];
 	printf("n은 몇개\n");
 	scanf("%d", &n);
 	
 	printf("정수 n개를 입력하시오\n");
 	for(i = 0; i < n; i++){
-		scanf("%d", &arr[n]);
+		scanf("%d", &arr[i]);
 	}
 	
-	printf("%d", findMax(arr, n, max)); 
+	printf("%d", findMax(arr, n)); 
 	
 	return 0;	
 }
